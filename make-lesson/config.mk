398b272511b0d00ca57e9a7abb8da5f1b40ba283
Seth Guberman <sguberman@gmail.com>
2017-04-25 13:58:54 +0000

    complete lesson with working config and Makefile

---
+++ make-lesson/config.mk
@@ -0,0 +1,12 @@
+# Count words script.
+COUNT_SRC=wordcount.py
+COUNT_EXE=python $(COUNT_SRC)
+
+# Test Zipf's rule.
+ZIPF_SRC=zipf_test.py
+ZIPF_EXE=python $(ZIPF_SRC)
+
+# Plot Zipf results.
+PLOT_SRC=plotcount.py
+PLOT_EXE=python $(PLOT_SRC)
+
